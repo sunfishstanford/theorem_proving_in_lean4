772122f392cfa259a26114b6c172914c9d879779
Francis Ho <10945841+sunfishstanford@users.noreply.github.com>
2026-01-17 18:13:19 -0800

    1

--- examples/FH notes for TPIL.lean
+++ examples/FH notes for TPIL.lean
@@ -1,6 +1,5 @@
 import Mathlib.Algebra.Ring.Defs
 import Mathlib.Data.Real.Basic
-import MIL.Common
 
 section test1
 
@@ -12,211 +11,39 @@ def f (α : Type u) (β : α → Type v) (a : α) (b : β a) : (a : α) × β a 
 
 def h1 (x : Nat) : Nat :=
   (f Type (fun α => α) Nat x).2
+
+#check Type 0
 /-
 f has four arguments
 1 = Type
 2 = (fun α => α)
 3 = Nat
 4 = x
-5
+
+α = Type 0: Type 1
+
+β = (fun α => α) : Type 0 → Type 0
+because the template says the domain is
+α, and we know that α = Type 0, and the function
+is an identity, so the codomain is also Type 0.
+
+a = Nat : Type 0, which is consistent with the template's requirement that a has type α
+
+β a = a = Nat : Type 0
+
+so b = x : Nat, which is consistent with the template's requirement that b has type β a = Nat
+
+
 
 -/
 
-
-#eval h1 5
-
 end test1
 
+section test2
+universe u
+def ident {α : Type u} (x : α) := x
+#check (ident)
+end test2
 
 #check List
 #check [2]
-
-def mylist : List nat := [2 3 4]
-
-
-#check Nat
-#check Nat → Nat
-#check Type
-#check Type 0
-#check Type 0 × Nat
-def F : Type → Type := List
-#check F
-#check Type → Type
-def G : Type 1 := Type → Type
-#check G
-#check 3
-def a : Nat := 3
-#check (2,4)
-def H : Nat × Nat := (4,12)
-
-section
-variable (R : Type*) [Ring R]
-
-#check (add_assoc : ∀ a b c : R, a + b + c = a + (b + c))
-#check (add_comm : ∀ a b : R, a + b = b + a)
-#check (zero_add : ∀ a : R, 0 + a = a)
-#check (neg_add_cancel : ∀ a : R, -a + a = 0)
-#check (mul_assoc : ∀ a b c : R, a * b * c = a * (b * c))
-#check (mul_one : ∀ a : R, a * 1 = a)
-#check (one_mul : ∀ a : R, 1 * a = a)
-#check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
-#check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
-
-end
-
-section
-variable (R : Type*) [CommRing R]
-variable (a b c d : R)
-
-example : c * b * a = b * (a * c) := by ring
-
-example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by ring
-
-example : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by ring
-
-example (hyp : c = d * a + b) (hyp' : b = a * d) : c = 2 * a * d := by
-  rw [hyp, hyp']
-  ring
-
-end
-
-namespace MyRing
-variable {R : Type*} [Ring R]
-
-theorem add_zero (a : R) : a + 0 = a := by rw [add_comm, zero_add]
-
-theorem add_right_neg (a : R) : a + -a = 0 := by rw [add_comm, neg_add_cancel]
-
-#check MyRing.add_zero
-#check add_zero
-
-end MyRing
-
-namespace MyRing
-variable {R : Type*} [Ring R]
-
-theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
-  rw [← add_assoc, neg_add_cancel, zero_add]
-
--- Prove these:
-theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
-  rw [add_assoc, add_right_neg, add_zero]
-
-theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  rw [← neg_add_cancel_left a b, h, neg_add_cancel_left]
-
-theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
-  rw [← add_neg_cancel_right a b,h,add_neg_cancel_right]
-
-theorem mul_zero (a : R) : a * 0 = 0 := by
-  have h : a * 0 + a * 0 = a * 0 + 0 := by
-    rw [← mul_add, add_zero, add_zero]
-  rw [add_left_cancel h]
-
-theorem zero_mul (a : R) : 0 * a = 0 := by
-  have h: 0*a + 0*a = 0*a + 0 := by
-    rw [← add_mul, add_zero,add_zero]
-  rw [add_left_cancel h]
-
-
-theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
-  rw [← neg_add_cancel_left a b,  h, add_zero]
-
--- theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
---   rw [←neg_add_cancel_right a b,add_assoc,add_comm (-b),← add_assoc,h,zero_add ]
-
--- theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
---   have g: b+a=0 := by
---     rw[add_comm,h]
---   symm
---   rw [neg_eq_of_add_eq_zero g]
-
-
-
-
-#check zero_sub
-#check sub_eq_add_neg
-#check neg_sub
-
-example: 0 - a = -a := by
-  apply zero_sub
-
-theorem neg_zero : (-0 : R) = 0 := by
-  apply neg_eq_of_add_eq_zero
-  rw [add_zero]
-
-theorem neg_neg (a : R) : - -a = a := by
-  rw [← zero_sub a ,neg_sub,sub_eq_add_neg,neg_zero,add_zero]
-#print neg_neg
-
-
-theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
-  have g: -a + -b =0 := by
-    rw[← zero_add (-a)]
-    nth_rw 1 [ ← h ]
-    rw[add_comm a,add_assoc b,add_neg_cancel,add_zero,add_neg_cancel]
-  rw [← neg_neg a]
-  apply neg_eq_of_add_eq_zero g
-
-
-end MyRing
-
--- Examples.
-section
-variable {R : Type*} [Ring R]
-
-example (a b : R) : a - b = a + -b :=
-  sub_eq_add_neg a b
-
-end
-
-example (a b : ℝ) : a - b = a + -b :=
-  rfl
-
-example (a b : ℝ) : a - b = a + -b := by
-  rfl
-
-namespace MyRing
-variable {R : Type*} [Ring R]
-
-theorem self_sub (a : R) : a - a = 0 := by
-  sorry
-
-theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
-  norm_num
-
-theorem two_mul (a : R) : 2 * a = a + a := by
-  sorry
-
-end MyRing
-
-section
-variable (A : Type*) [AddGroup A]
-
-#check (add_assoc : ∀ a b c : A, a + b + c = a + (b + c))
-#check (zero_add : ∀ a : A, 0 + a = a)
-#check (neg_add_cancel : ∀ a : A, -a + a = 0)
-
-end
-
-section
-variable {G : Type*} [Group G]
-
-#check (mul_assoc : ∀ a b c : G, a * b * c = a * (b * c))
-#check (one_mul : ∀ a : G, 1 * a = a)
-#check (inv_mul_cancel : ∀ a : G, a⁻¹ * a = 1)
-
-namespace MyGroup
-
-theorem mul_inv_cancel (a : G) : a * a⁻¹ = 1 := by
-  sorry
-
-theorem mul_one (a : G) : a * 1 = a := by
-  sorry
-
-theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
-  sorry
-
-end MyGroup
-
-end
